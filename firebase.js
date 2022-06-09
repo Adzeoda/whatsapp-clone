99e68ecfdd7eabb994b642e295478cfec5d24a1c
Adjeoda <andydogah@gmail.com>
2022-06-09 01:23:24 +0000

    npm and yarn not starting

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAzBZzuYSj258uuvhur2rVF8Jv7qzPFaNQ",
+  authDomain: "whatsapp-clone-dd4d9.firebaseapp.com",
+  projectId: "whatsapp-clone-dd4d9",
+  storageBucket: "whatsapp-clone-dd4d9.appspot.com",
+  messagingSenderId: "19426454251",
+  appId: "1:19426454251:web:7019176e8612c334132cae",
+  measurementId: "G-EFP3G9BF0X",
+};
